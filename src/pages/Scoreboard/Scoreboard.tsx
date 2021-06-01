21fc4ff53d0d97dd0f54ee2e5f20a2c0030e69fb
Gaurav <gauravdey625@gmail.com>
2021-06-01 23:56:21 +0530

    Added loaders

--- src/pages/Scoreboard/Scoreboard.tsx
+++ src/pages/Scoreboard/Scoreboard.tsx
@@ -4,38 +4,53 @@ import './scoreboard.css'
 import { sortScorers } from './scoreboard.utils'
 import axios from 'axios'
 import { Score } from './scoreboard.types'
+import Loader from "react-loader-spinner";
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 export function Scoreboard() {
     const [scores, SetScores] = useState([] as Score[])
+    const [isReceived, setReceived] = useState(false)
     useEffect(() => {
         (async function () {
             await axios.get("https://quizzerd-backend.herokuapp.com/score/ShowAllScore"
             )
-                .then((response) => SetScores(sortScorers(response.data)))
+                .then((response) => {
+                    SetScores(sortScorers(response.data))
+                    setReceived(true)
+                })
         })()
     }, [])
     const navigate = useNavigate()
     return (
         <div className='scoreboard'>
-            <h1 className='heading'>Leaderboard</h1>
-            {
-                scores.length > 0 ?
-                    <table>
-                        <tr>
-                            <th>Name</th>
-                            <th>Score</th>
-                            <th>Genre</th>
-                        </tr>
-                        {scores.map(person =>
-                            <tr>
-                                <td>{person.username}</td>
-                                <td>{person.score}</td>
-                                <td>{person.genre}</td>
-                            </tr>)
-                        }
-                    </table>
-                    : <h1>No records found</h1>
+            {isReceived ?
+                <div>
+                    <h1 className='heading'>Leaderboard</h1>
+                    {
+                        scores.length > 0 ?
+                            <table>
+                                <tr>
+                                    <th>Name</th>
+                                    <th>Score</th>
+                                    <th>Genre</th>
+                                </tr>
+                                {scores.map(person =>
+                                    <tr>
+                                        <td>{person.username}</td>
+                                        <td>{person.score}</td>
+                                        <td>{person.genre}</td>
+                                    </tr>)
+                                }
+                            </table>
+                            : <h1>No records found</h1>
+                    }
+                    <button className='btn-back' onClick={() => navigate('/')}>Back</button>
+                </div>
+                : <Loader type="Puff"
+                    color="#00BFFF"
+                    height={100}
+                    width={100}
+                />
             }
-            <button className='btn-back' onClick={() => navigate('/')}>Back</button>
         </div>
     );
 }
--- src/pages/UserDetails/userdetails.tsx
+++ src/pages/UserDetails/userdetails.tsx
@@ -3,6 +3,8 @@ import { useEffect, useState } from 'react'
 import axios from 'axios'
 import { User, Score } from './userdetails.types'
 import { useAuth } from '../../Context/AuthProvider'
+import Loader from "react-loader-spinner";
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 export const Userdetails = () => {
     const [user, SetUser] = useState({} as User)
     const [score, SetScore] = useState([] as Score[])
@@ -29,13 +31,21 @@ export const Userdetails = () => {
 
     return (
         <div className="user">
-            <div className="user-details">
-                <h2>Username: {user.username}</h2>
-                <h2>Email: {user.email}</h2>
-                <h2>Quizzes Attempted: {score.length}</h2>
-                <h2>Highest Score: </h2>
-                <button className="btn-logout" onClick={LogOut}>Log Out</button>
-            </div>
+            {
+                user.username ? <div className="user-details">
+                    <h2>Username: {user.username}</h2>
+                    <h2>Email: {user.email}</h2>
+                    <h2>Quizzes Attempted: {score.length}</h2>
+                    <h2>Highest Score: </h2>
+                    <button className="btn-logout" onClick={LogOut}>Log Out</button>
+                </div>
+                    : <Loader type="Puff"
+                        color="#00BFFF"
+                        height={100}
+                        width={100}
+                    />
+            }
+
         </div>
     );
 }
